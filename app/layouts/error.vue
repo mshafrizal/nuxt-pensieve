b99095082ac8694a38b6f2d5d0288bd405c8056e
Muhammad Shafrizal <30531451+mshafrizal@users.noreply.github.com>
2022-11-30 14:17:12 +0000

    feat: add 404 page

---
+++ app/layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="flex flex-col w-full justify-stretch items-center min-h-screen flex-grow-1">
+    <h1 class="text-5xl">Whoops, wrong way...</h1>
+    <NuxtLink to="/">Click here to go home</NuxtLink>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component({
+  // Called to know which transition to apply
+  transition() {
+    return 'slide-left';
+  },
+  layout: 'home',
+})
+export default class Error extends Vue {}
+</script>
