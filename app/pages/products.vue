08187b826d7dd8caaefe333e5b5b91beaee6e4a7
Muhammad Shafrizal <30531451+mshafrizal@users.noreply.github.com>
2022-12-14 18:35:36 +0000

    feat: update products page

--- app/pages/products.vue
+++ app/pages/products.vue
@@ -2,61 +2,55 @@
   <section class="career">
     <div class="hero">
       <h1 class="hero__text container mx-auto text-white">
-        Pensieve helps governments solve their most complex challenges in digital transformation
+        Goodbye Data Complexities.
+        <br />
+        <br />
+        Hello AI-Powered Government.
       </h1>
     </div>
 
-    <div class="content pt-24 pb-64 flex flex-col">
-      <div class="container mx-auto flex flex-col sm:flex-row justify-between mb-48">
-        <div class="w-1/3 flex flex-col">
-          <h2 class="text-6xl leading-none mb-10 font-thin">
-            How It Works
-          </h2>
-          <h3 class="hiw-title">
-            4 <br />
-            User Layer Interface
-          </h3>
-          <p class="hiw-description">
-            Intuitive Desktop and mobile apps with a human-centric approach
-          </p>
-          <h3 class="hiw-title">
-            3 <br />
-            Data Analytics for Decision-Making Support
-          </h3>
-          <p class="hiw-description">
-            Our AI/ML algorithms augment human intelligence effectively and efficiently
-          </p>
-          <h3 class="hiw-title">
-            2 <br />
-            Data Integration and Engineering
-          </h3>
-          <p class="hiw-description">
-            Integration and engineering layer that connects multiple data sources.
-          </p>
-          <h3 class="hiw-title">
-            1 <br />
-            Data Assets and Acquisition
-          </h3>
-          <p class="hiw-description">
-            Multiple data sources, sensors, and legacy systems exist in silos.
-          </p>
-        </div>
-        <div class="w-2/3 flex justify-end items-start">
-          <lottie-player
-            src="/animation/career-1.json"
-            ref="lottie"
-            id="lottieBtn"
-            loop
-            autoplay
-          ></lottie-player>
-        </div>
+    <div class="content pt-24 pt-32 pb-32 flex flex-col">
+      <div class="container mx-auto flex flex-col mb-48">
+        <h2 class="text-4xl font-thin" style="width: 875px; margin: 0 auto;">What We Deliver</h2>
+        <lottie-player
+          src="/animation/stack-4.json"
+          ref="lottie_4"
+          id="stack-4"
+          loop
+          autoplay
+          style="z-index: 4;"
+        ></lottie-player>
+        <lottie-player
+          src="/animation/stack-3.json"
+          ref="lottie_3"
+          id="stack-3"
+          loop
+          autoplay
+          style="z-index: 3; margin-top: -200px;"
+        ></lottie-player>
+        <lottie-player
+          src="/animation/stack-2.json"
+          ref="lottie_2"
+          id="stack-2"
+          loop
+          autoplay
+          style="z-index: 2; margin-top: -240px;"
+        ></lottie-player>
+        <lottie-player
+          src="/animation/stack-1.json"
+          ref="lottie_1"
+          id="stack-1"
+          loop
+          autoplay
+          style="z-index: 1; margin-top: -200px;"
+        ></lottie-player>
       </div>
     </div>
     <div class="products">
       <div class="container mx-auto">
-        <h2 class="text-5xl font-thin mb-64">
-          Pensieve enables a new era of e-government/ digital government that provides great public
-          service at scale.
+        <h2 class="text-5xl font-thin mb-32">
+          Explore tomorrow’s AI-driven solutions finely crafted for digital government and
+          enterprise
         </h2>
         <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-10">
           <div v-for="(service, i) in services" class="flex flex-col items-center" :key="i">
@@ -101,7 +95,7 @@ export default class Career extends Vue {
   transform: translate(0, -50%);
   color: white;
   width: 100%;
-  max-width: 936px;
+  max-width: 1280px;
   font-size: 80px;
   font-weight: 200;
   line-height: 1;
