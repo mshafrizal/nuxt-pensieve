4d1d9910b4741b9c22d8a9fdeaf2fa03d1a4c6cc
Muhammad Shafrizal <mshafrizal@gmail.com>
2022-12-22 02:11:03 +0000

    fix: footer mobile view

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -1,27 +1,31 @@
 <template>
-  <footer class="footer flex items-center">
-    <div class="container mx-auto flex items-center w-full">
-      <img
-        src="https://ik.imagekit.io/vin64b9qvp9/pensieveassets/uploads/footer-1-P__Mq5GDDmgc.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1669442171601"
-        alt="Pensieve P Logo"
-        class="w-4 h-4 mr-8"
-      />
-      <p class="flex-grow">2022 Pensieve • All Rights Reserved</p>
-      <img
-        src="https://ik.imagekit.io/vin64b9qvp9/pensieveassets/uploads/footer-2-instagram_ct-jXfnbq.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1669442175603"
-        alt="Instagram logo"
-        class="w-4 h-4 mr-8"
-      />
-      <img
-        src="https://ik.imagekit.io/vin64b9qvp9/pensieveassets/uploads/footer-3-kalibrr_yfoyaFJJW.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1669442184159"
-        alt="Kalibrr logo"
-        class="w-4 h-4 mr-8"
-      />
-      <img
-        src="https://ik.imagekit.io/vin64b9qvp9/pensieveassets/uploads/footer-4-linkedin_rd7LocaEG.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1669442180112"
-        alt="Linkedin logo"
-        class="w-4 h-4"
-      />
+  <footer class="footer flex items-center px-6 sm:px-0 py-10 sm:py-0">
+    <div class="container mx-auto flex flex-col items-center w-full">
+      <div class="flex justify-between w-full">
+        <img
+          src="https://ik.imagekit.io/vin64b9qvp9/pensieveassets/uploads/pensieve-logo-black_wwWueRMhu.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1669442167496"
+          alt="Pensieve P Logo"
+          class="w-32"
+        />
+        <div class="flex gap-6">
+          <img
+            src="https://ik.imagekit.io/vin64b9qvp9/pensieveassets/uploads/footer-2-instagram_ct-jXfnbq.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1669442175603"
+            alt="Instagram logo"
+            class="w-4 h-4 flex-grow-0"
+          />
+          <img
+            src="https://ik.imagekit.io/vin64b9qvp9/pensieveassets/uploads/footer-3-kalibrr_yfoyaFJJW.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1669442184159"
+            alt="Kalibrr logo"
+            class="w-4 h-4 flex-grow-0"
+          />
+          <img
+            src="https://ik.imagekit.io/vin64b9qvp9/pensieveassets/uploads/footer-4-linkedin_rd7LocaEG.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1669442180112"
+            alt="Linkedin logo"
+            class="w-4 h-4 flex-grow-0"
+          />
+        </div>
+      </div>
+      <p class="text-xs">2022 Pensieve • All Rights Reserved</p>
     </div>
   </footer>
 </template>
