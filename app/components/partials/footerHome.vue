5242cf332f04992ec0367ccabe9d53cfa68a6048
Muhammad Shafrizal <30531451+mshafrizal@users.noreply.github.com>
2023-01-04 04:01:29 +0000

    fix: rendering wrong component when navigate back to home

--- app/components/partials/footerHome.vue
+++ app/components/partials/footerHome.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer fixed bottom-0 w-full px-8 py-2 md:py-6 md:px-10">
+  <footer class="footer-home fixed bottom-0 w-full px-8 py-2 md:py-6 md:px-10">
     <p class="container mx-auto">2022 Pensieve • All Rights Reserved</p>
   </footer>
 </template>
@@ -12,7 +12,7 @@ export default class Footer extends Vue {}
 </script>
 
 <style lang="scss">
-.footer {
+.footer-home {
   background-color: $pgreylight;
 }
 </style>
